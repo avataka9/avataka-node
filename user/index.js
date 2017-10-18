989d7ea6239d333bf1dfab2e04472ae5de38bb1b
avataka <avataka9@gmail.com>
2017-10-18 10:41:14 +0300

    user.js and ru.json is moved to user

--- user/index.js
+++ user/index.js
@@ -1,9 +1,11 @@
+var phrases = require('./ru');
+
 function User(name) {
   this.name = name;
 }
 
 User.prototype.hello = function(who) {
-  console.log("Hello, " + who.name);
+  console.log(phrases.Hello + ", " + who.name);
 };
 
 console.log("user.js is required!");
